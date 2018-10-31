368b0654fa4a80a709dfcd01cd436675bbfa65b1
wengzhongjie <1310120194@qq.com>
2018-10-31 13:05:17 +0800

    格式化了代码

--- src/main/java/com/weng/maven/Demo.java
+++ src/main/java/com/weng/maven/Demo.java
@@ -6,9 +6,9 @@ public class Demo {
 
         System.out.println("添加一个输出语句");
     }
-    
-    public void run(){
-          System.out.println("添加了一个run方法");
+
+    public void run() {
+        System.out.println("添加了一个run方法");
         System.out.println("在run方法中添加了次要语句..");
     }
 }

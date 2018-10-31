4d7269bcab9582fcd16a82c5d31f35cc826b1977
wengzhongjie <1310120194@qq.com>
2018-10-31 12:56:28 +0800

    添加一个输出语句

--- src/main/java/com/weng/maven/Demo.java
+++ src/main/java/com/weng/maven/Demo.java
@@ -3,5 +3,7 @@ package com.weng.maven;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("执行一个主方法..");
+
+        System.out.println("添加一个输出语句");
     }
 }

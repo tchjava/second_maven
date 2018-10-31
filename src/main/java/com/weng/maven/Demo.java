cb6ee9fed3a96f5b0c8687b50afc2314a0a0841a
wengzhongjie <1310120194@qq.com>
2018-10-31 13:08:57 +0800

    添加了fun1方法

--- src/main/java/com/weng/maven/Demo.java
+++ src/main/java/com/weng/maven/Demo.java
@@ -1,5 +1,6 @@
 package com.weng.maven;
 
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("执行一个主方法..");
@@ -10,5 +11,9 @@ public class Demo {
     public void run() {
         System.out.println("添加了一个run方法");
         System.out.println("在run方法中添加了次要语句..");
+    }
+
+    public void fun1(){
+        System.out.println("添加了fun1方法");
     }
 }

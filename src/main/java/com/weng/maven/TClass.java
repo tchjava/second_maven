6dda0529c122415898f00c9bb038e102799d17ab
wengzhongjie <1310120194@qq.com>
2018-10-31 13:53:26 +0800

    添加了一个name字段和set/get方法

--- src/main/java/com/weng/maven/TClass.java
+++ src/main/java/com/weng/maven/TClass.java
@@ -2,4 +2,21 @@ package com.weng.maven;
 
 public class TClass {
     private Long cid;
+    private Long name;
+
+    public Long getCid() {
+        return cid;
+    }
+
+    public void setCid(Long cid) {
+        this.cid = cid;
+    }
+
+    public Long getName() {
+        return name;
+    }
+
+    public void setName(Long name) {
+        this.name = name;
+    }
 }

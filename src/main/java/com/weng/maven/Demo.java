ef7ce2c9f8915f0b552873eb8db9fb22e1db3c8c
tchjava <43031306+tchjava@users.noreply.github.com>
2018-10-31 12:40:19 +0800

    Update Demo.java
    添加了一个run方法

--- src/main/java/com/weng/maven/Demo.java
+++ src/main/java/com/weng/maven/Demo.java
@@ -4,4 +4,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("执行一个主方法..");
     }
+    
+    public void run(){
+          System.out.println("添加了一个run方法");
+    }
 }

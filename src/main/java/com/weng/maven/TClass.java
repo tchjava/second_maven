9e4637a0526a61c2ca73f6fe92d5c9e64b0808bd
wengzhongjie <1310120194@qq.com>
2018-10-31 13:54:33 +0800

    添加了一个address字段和set/get方法

--- src/main/java/com/weng/maven/TClass.java
+++ src/main/java/com/weng/maven/TClass.java
@@ -2,4 +2,13 @@ package com.weng.maven;
 
 public class TClass {
     private Long cid;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
